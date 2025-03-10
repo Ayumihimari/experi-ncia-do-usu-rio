89b91dfaaec9227935cb09130c5fa1d6fab9836a
Ellen Luiza <137297820+Ayumihimari@users.noreply.github.com>
2025-03-10 08:26:22 -0300

    html

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ botaoDeAcessibilidade.addEventListener('click', function (){
    alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste')
 })
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.js"></script>
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
